925265680407022ffa34599ec7e2aa52629f1e51
Douglas Menchon <114234741+Dougmgm@users.noreply.github.com>
2024-09-27 09:10:26 -0300

    Add files via upload

---
+++ ProjetoCadastros/Enum/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCadastros.Enum
+{
+    public enum Status
+    {
+        Pendente,
+        Pago,
+        Enviado,
+        Recebido
+    }
+}
---
+++ ProjetoCadastros/Enum/TipoPagamentoEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCadastros.Enum
+{
+    public enum TipoPagamentoEnum
+    {        
+        Dinheiro,
+        Debito,
+        Credito,
+        Pix,
+        Boleto
+    }
+}
